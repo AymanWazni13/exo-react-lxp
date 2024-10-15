52cc6129686144f5a7fe5ecf7ddfba07e6318ec4
AymanWazni13 <ayman.wazni@next-u.fr>
2024-10-15 22:37:26 +0200

    change price and name

--- src/components/card.jsx
+++ src/components/card.jsx
@@ -1,14 +1,14 @@
-export function Cart (){
+export function Cart ({name, price}){
     return (<div>
         <article>
     <header>
-      <h2>Air MAX DN</h2>
+      <h2>{name}</h2>
     </header>
     <figure>
       <img src="https://nikeuyprod.vtexassets.com/arquivos/ids/256879-1200-1200?width=1200&height=1200&aspect=true" alt="Description of the product"/>
     </figure>
     <section>
-      <p>Price: $189.99</p>
+      <p>Price: ${price}</p>
     </section>
     <footer>
       <button>Add to Cart</button>
